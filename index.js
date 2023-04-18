cad53692cf038aaa671a25f2e78ab1648fa16c0b
Baatile <batilelanga@hotmail.com>
2023-04-18 22:23:33 +0200

    rest api

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const express = require('express')
 const app = express()
 const port = 5005
 
-app.get('/', (req, res) => {
 
-    app.get('/', (req, res) => {
+
+app.get('/', (req, res) => {
         res.send('Hello World!')
       })
      
-})
+
+
+app.post('/postmessage', (req, res) => {
+ res.send("message")
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
